740bb12c8a2d30a7c81e196872f98b892b3615ea
Atul V Chandran <atulvc2001@gmail.com>
2024-05-04 11:05:25 +0530

    completed duck db basic tutorial

--- notebook.py
+++ notebook.py
@@ -47,15 +47,62 @@ conn.execute("""SELECT * FROM df WHERE "Order ID"='295665'""").df()
 # It is not ideal to create a virtual table all the time, it's better to create a table directly if there isn't much complexity
 conn.execute("""
 CREATE OR REPLACE TABLE sales AS
-	SELECT
-		"Order ID"::INTEGER AS order_id,
-		Product AS product,
-		"Quantity Ordered"::INTEGER AS quantity,
-		Price::DECIMAL AS price_each,
-		strptime("Order Date", '%m/%d/%Y %H:%M')::DATE as order_date,
-		"Purchase Address" AS purchase_address
-	FROM df
-	WHERE
-		TRY_CAST("Order ID" AS INTEGER) NOTNULL
+SELECT
+    "Order ID"::INTEGER AS order_id,
+    Product AS product,
+    "Quantity Ordered"::INTEGER AS quantity,
+    REPLACE("Price", ',', '')::DECIMAL AS price_each,
+    "Order Date"::DATE AS order_date,
+    "Purchase Address" AS purchase_address
+FROM df
+WHERE
+    TRY_CAST("Order ID" AS INTEGER) IS NOT NULL;
 """)
+# %%
+# Exclude data from select statement
+conn.execute("""
+	SELECT 
+		* EXCLUDE (product, order_date, purchase_address)
+	FROM sales
+	""").df()
+# %%
+# The COlumns Expression
+conn.execute("""
+	SELECT 
+        MIN(COLUMNS(* EXCLUDE(product, order_date, purchase_address)))
+    FROM sales
+""")
+# %%
+# Creating a view for aggregrating data
+conn.execute("""
+	CREATE OR REPLACE VIEW aggregated_sales AS
+	SELECT
+		order_id,
+		COUNT(1) as nb_orders,
+		MONTH(order_date) as month,
+		str_split(purchase_address, ',')[2] AS city,
+		SUM(quantity * price_each) AS revenue
+	FROM sales
+	GROUP BY ALL
+""")
+# %%
+conn.execute("FROM aggregated_sales").df()
+# %%
+conn.execute("""
+	SELECT 
+        city,
+        SUM(revenue) as total
+    FROM aggregated_sales
+    GROUP BY city
+    ORDER BY total DESC
+""").df()
+# %%
+# Parquet file
+conn.execute("""
+	COPY (FROM aggregated_sales) 
+    TO 'aggregated_sales.parquet' (FORMAT 'parquet')
+""")
+# %%
+# Reading Parquet file
+conn.execute("FROM aggregated_sales.parquet").df()
 # %%
